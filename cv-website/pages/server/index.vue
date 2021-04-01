2df8a8018faaad20297e41cc66622b6929d3414f
Luís Simões <danielsantos63@gmail.com>
2021-04-01 23:00:54 +0100

    attempt to get server data on static page

---
+++ cv-website/pages/server/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <div>{{ post.title }}</div>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: []
+    };
+  },
+  methods: {
+    loadPosts() {
+      this.$axios
+        .$get(
+          "https://nuxt-course-ed535-default-rtdb.firebaseio.com/posts.json"
+        )
+        .then(res => {
+          for (const key in res) {
+            this.posts.push(res[key]);
+          }
+        });
+    }
+  },
+  created() {
+    this.loadPosts();
+    console.log(this.posts);
+  }
+};
+</script>
+
+<style></style>
