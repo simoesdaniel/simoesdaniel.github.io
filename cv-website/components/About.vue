011251852b842212e62cd34cc4966db078c3e288
Luís Simões <danielsantos63@gmail.com>
2021-04-01 22:23:25 +0100

    code cleaning

---
+++ cv-website/components/About.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="text-center text-gray-600">
+    <p class="text-md py-1">
+      I was born in Barcelos, Portugal
+      {{ +new Date().getFullYear() - 1989 }} years ago.
+    </p>
+    <p class="text-md py-1">
+      I'm a tech nerd, hyped about the next big thing. And yeah, I code for a
+      living.
+    </p>
+    <p class="text-md py-1">
+      I started my journey as a web developer and I grew into a full-stack guy
+      who works with a tone of different technologies both frontend and backend,
+      but I would say that my heart always will fall on the frontend.
+    </p>
+    <p class="text-md py-1">
+      I've passion for learning new things, embrace challenges and I also
+      consider myself a problem solver.
+    </p>
+    <p class="text-md py-1">
+      I enjoy gaming, listen to music and (as everyone else in the last 5 years)
+      love to listen to podcast on a variety of subjects (tech, coding, comedy,
+      politics).
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
